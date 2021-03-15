e7082e9f52bd4f2590bad54bc164d9ad66c073c4
cristianabote <cristianamihaelaplesa@gmail.com>
2021-03-15 11:10:30 +0200

    add .gitignore file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Implementati o clasa PuncteDoiD care sa contina: un contructor care primeste 2 numere reale (de tip float)ce reprezinta coordonatele
+        // Implementati o clasa PuncteDoiD care sa contina:
+        // un contructor care primeste 2 numere reale (de tip float)ce reprezinta coordonatele
         //O metoda modifica coordonate ce primeste doua numere reale si modifica cele doua coordonate ale punctului ,
-         // O functie de afisare a unui punct care va afisa punctul in formatul urmator : (x, y)
+        // O functie de afisare a unui punct care va afisa punctul in formatul urmator : (x, y)
 
         Scanner scan = new Scanner(System.in);
         System.out.println("Introduceti coordonatele punctului dorit");
